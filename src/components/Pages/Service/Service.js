ca5a31e60953b80130ad9641686d1d2f0e198123
samiulpro <samiul15-13051@diu.edu.bd>
2022-04-27 06:48:55 +0600

    Update: Updated single product page

--- src/components/Pages/Service/Service.js
+++ src/components/Pages/Service/Service.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import Data from '../../../FakeDB/service.json'
 const Service = () => {
     const {id}=useParams();
-    const detail=Data.find(item=>item.id===id)
+    const detail=Data?.find(item=>item.id==id)
     console.log(detail)
   return (
     <div className="w-5/6 my-12 mx-auto flex grid items-center grid-cols-1 md:grid-cols-2 gap-5">
@@ -11,7 +11,17 @@ const Service = () => {
             <img src={detail.image} className=" md:w-3/6 md:mx-auto" alt="service"/>
         </div>
         <div>
-        {detail.name}
+        <h1 className='text-4xl text-green-800'>{detail.name}</h1>
+        <div className='mt-2'>
+        <p>{detail.desc}</p>
+        </div>
+        <div className='mt-2'>
+        <p>{detail.sku}</p>
+        <p>Ratinngs: {detail.ratings}/5</p>
+        </div>
+        <div className='mt-5'>
+          <Link to="/" className='text-white px-3 py-2 bg-green-600'>Place Order</Link>
+        </div>
         </div>
         </div>
   )
